b5152819f607629940913853655fddb722cde8e8
jbleich <jbleich89@gmail.com>
2014-05-09 14:38:17 -0400

    updates

--- kernReg/R/kpca_regression.R
+++ kernReg/R/kpca_regression.R
@@ -12,13 +12,50 @@ kpca_regression = function(y, kpca_object, num_pcs){
 }
 
 
-kpca_logistic_regression = function(y, kpca_object, num_pcs){
+kpca_logistic_regression = function(y, kpca_object, num_pcs, weights = NULL){
   if(class(kpca_object) != "kernel_pca") stop("Need a kernel pca object. Call kernel_pca().")
   X = kpca_object$pc_mat[, 1 : num_pcs]
-  mod = glm(y ~ ., data = as.data.frame(X), family = "binomial")
+  mod = glm(y ~ ., data = as.data.frame(X), family = "binomial", weights = weights)
   mod$ols = FALSE
   mod$kpca_object = kpca_object
   mod$num_pcs = num_pcs
   class(mod) = c("glm", "lm", "kernReg")
   mod
 }
+
+plot_perf = function(ytrain, xtrain, ytest, xtest, kpca_object, pc_seq = NULL, fp_to_fn_cost = 1, threshold = .5, weights = NULL){
+  if(class(kpca_object) != "kernel_pca") stop("Need a kernel pca object. Call kernel_pca().")
+  if(is.null(pc_seq)){
+    num_pcs = get_num_pcs(kpca_object = kpca_object, frac_var_to_explain = .95)
+    pc_seq = seq(1, num_pcs, by = 20)
+  }
+  
+  err_mat = matrix(nrow = length(pc_seq), ncol = 3)
+  colnames(err_mat) = c("Class 0", "Class 1", "Cost-Weighted")
+  
+  for(i in 1 : length(pc_seq)){
+    mod = kpca_logistic_regression(y = ytrain, kpca_object = kpca_object, num_pcs = pc_seq[i] , weights = weights)
+    preds = kernReg_predict(object = mod, new_data = xtest, training_data = xtrain)
+    tab = table(factor(ytest, levels = c(0,1)), factor(as.numeric(preds > threshold), levels = c(0,1)))
+    fp = tab[1,2]/sum(tab[1,]) ##FP
+    fn = tab[2,1]/sum(tab[2,]) ##FN
+    cost = fp * sum(tab[1,]) * fp_to_fn_cost + fn * sum(tab[2,])
+    ##put cost here
+    err_mat[i,] = c(fp, fn, cost)
+    print(i)
+  }
+
+  plot(pc_seq, err_mat[,1], type = "o", col = "blue", ylim = c(0,1))
+  points(pc_seq, err_mat[,2], type = "o", col = "red")
+  #plot(pc_seq, err_mat[,3], type = "l")
+  invisible(err_mat)
+}
+
+# kpca_object = kpca_obj
+# xtrain = Split2Design
+# xtest = Split3Design
+# ytrain = Fail2
+# ytest = Fail3
+# fp_to_fn_cost = 2
+# threshold = .67
+# weights = NULL
--- kernReg/R/predict.R
+++ kernReg/R/predict.R
@@ -2,14 +2,14 @@ kernReg_predict = function(object, new_data, training_data){
   if(!any(class(object)== "kernReg")) stop("Need a kernReg object.")
   X = matrix(NA)
   if(object$ols == T){
-    X = object$model[ ,-1]
+    X = object$model[ ,-1, drop = F]
   }else{
-    X = object$data
+    X = as.matrix(object$data)
   }
 
   k_vecs = t(sapply(1 : nrow(new_data), function(s) K_vector(xstar = new_data[s,], X = training_data, fun = object$kpca_object$K_object$fun, params = object$kpca_object$K_object$params)))
   k_vecs_c = sapply(1 : nrow(new_data), function(s) center_kernel_test_vec(k_vec = k_vecs[s,], K = object$kpca_object$K_object$K))
-  rotated_kvecs = (t(k_vecs_c) %*% object$kpca_object$eigenvecs)[ , 1 : object$num_pcs ]
+  rotated_kvecs = (t(k_vecs_c) %*% object$kpca_object$eigenvecs)[ , 1 : object$num_pcs, drop = F ]
   rotated_kvecs = as.matrix(rotated_kvecs)
   if(ncol(X)!= ncol(rotated_kvecs)) stop("Need same number of columns in newdata as original data.")
   colnames(rotated_kvecs) = colnames(X)
