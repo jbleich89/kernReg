f9d324064d1d1a94f1cc59d1a3f67ec4bf415b40
Adam Kapelner <kapelner@gmail.com>
2014-06-25 14:08:50 -0400

    decomped for ease and fixed some bugs

---
+++ kernReg/R/eval_test_data_from_explore_and_build_final_model.R
@@ -0,0 +1,110 @@
+#' Evaluate Test Data
+#'
+#' After a "satisfactory" model is selected by the user using the \code{explore_kpcr_models} function,
+#' we now predict on the test data to get a glimpse into this model's future out-of-sample performance. 
+#' Warning: once this is done, you cannot "go back" and "try" to assess performance on new kernels as this
+#' would then be snooping. Run this function when you are ready to close the books on this data set and never
+#' look back.
+#' 
+#' @param explore_kpclr_obj 		This object is built from \code{explore_kplcr_models}. We assume the user has updated 
+#' 									this object with a satisfactory model by settings \code{winning_kernel_num} to denote 
+#' 									which kernel is selected for the final model and setting \code{winning_rho_num} to 
+#' 									denote which proportion of the variance of the kernel matrix is selected for the 
+#' 									final model.
+#' 
+#' @return 							An expanded \code{explore_kpclr} list object with new entries
+#' 									that contain information about the performance of the final model on the
+#' 									test data: \code{test_confusion}, the confusion matrix of the test data; 
+#' 									\code{test_confusion_proportions}, the confusion matrix of the test data
+#' 									as proportions of the number of test observations; \code{test_misclassification_error}, 
+#' 									the total misclassification error of the test data and \code{test_weighted_cost},  
+#' 									the cost of the errors made as defined by the \code{fn_cost} and \code{fp_cost}
+#' 									specified by the user when constructing the model via \code{explore_kplcr_models}.
+#' 
+#' @seealso 						code{explore_kplcr_models}
+#' @author 							Adam Kapelner and Justin Bleich
+#' @export
+eval_winning_lr_model_on_test_data = function(explore_kpclr_obj){
+	#predict the model on test data and build a confusion matrix
+	#predict the model on training and validation data
+	winning_kernel_info = explore_kpclr_obj$kernel_list[[explore_kpclr_obj$winning_kernel_num]]
+	X_train_and_validate = rbind(explore_kpclr_obj$X_train, explore_kpclr_obj$X_validate)
+	kpca = build_kpca_object(X_train_and_validate, winning_kernel_info$kernel_type, winning_kernel_info$params)
+	y_train_and_validate = c(explore_kpclr_obj$y_train, explore_kpclr_obj$y_validate)
+	weights = weights_for_kpclr(y_train_and_validate, explore_kpclr_obj$fn_cost / explore_kpclr_obj$fp_cost)
+	winning_model = kpclr(kpca, y_train_and_validate, frac_var = explore_kpclr_obj$rho_seq[explore_kpclr_obj$winning_rho_num], weights = weights, family = explore_kpclr_obj$family)	
+	y_test_hat = predict(winning_model, explore_kpclr_obj$X_test)
+	test_confusion = table(explore_kpclr_obj$y_test, ifelse(y_test_hat > 0.5, 1, 0)) ###FIX LATER!!!
+	#pass back the data
+	explore_kpclr_obj[["test_confusion"]] = test_confusion
+	explore_kpclr_obj[["test_confusion_proportions"]] = test_confusion / explore_kpclr_obj$n_test
+	explore_kpclr_obj[["test_misclassification_error"]] = (test_confusion[1, 2] + test_confusion[2, 1]) / explore_kpclr_obj$n_test
+	explore_kpclr_obj[["test_weighted_cost"]] = test_confusion[2, 1] * explore_kpclr_obj$fn_cost + test_confusion[1, 2] * explore_kpclr_obj$fp_cost
+	explore_kpclr_obj
+}
+
+#' Evaluate Test Data
+#'
+#' After a "satisfactory" model is selected by the user using the \code{explore_kpcr_models} function,
+#' we now predict on the test data to get a glimpse into this model's future out-of-sample performance. 
+#' Warning: once this is done, you cannot "go back" and "try" to assess performance on new kernels as this
+#' would then be snooping. Run this function when you are ready to close the books on this data set and never
+#' look back.
+#' 
+#' @param explore_kpcr 				This object is built from \code{explore_kpcr_models}. We assume the user has updated 
+#' 									this object with a satisfactory model by settings \code{winning_kernel_num} to denote 
+#' 									which kernel is selected for the final model and setting \code{winning_rho_num} to 
+#' 									denote which proportion of the variance of the kernel matrix is selected for the 
+#' 									final model.
+#'
+#' @return 							An expanded \code{explore_kpcr} list object with new entries
+#' 									that contain information about the performance of the final model on the
+#' 									test data: \code{L2_err}, the sum of squared error; \code{rmse}, the root 
+#' 									mean squared error and \code{L1_err}, the sum of absolute error.
+#' 
+#' @seealso 						\code{\link{explore_kplr_models}}
+#' @author 							Adam Kapelner and Justin Bleich
+#' @export
+eval_winning_r_model_on_test_data = function(explore_kpcr){
+	#predict the model on training and validation data
+	winning_kernel_info = explore_kpcr$kernel_list[[explore_kpcr$winning_kernel_num]]
+	X_train_and_validate = rbind(explore_kpcr$X_train, explore_kpcr$X_validate)
+	kpca = build_kpca_object(X_train_and_validate, winning_kernel_info$kernel_type, winning_kernel_info$params)
+	y_train_and_validate = c(explore_kpcr$y_train, explore_kpcr$y_validate)
+	winning_model = kpcr(kpca, y_train_and_validate, frac_var = explore_kpcr$rho_seq[explore_kpcr$winning_rho_num])
+	y_test_hat = predict(winning_model, explore_kpcr$X_test)
+	#pass back the performance data
+	sse = sum((y_test_hat - exploration_kpclr$y_test)^2)
+	exploration_kpclr[["L2_err"]] = sse
+	exploration_kpclr[["rmse"]] = sqrt(sse / explore_kpcr$n_test)	
+	exploration_kpclr[["L1_err"]] = sum(abs(y_test_hat - explore_kpcr$y_test))
+	exploration_kpclr
+}
+
+#' Create Final Kernel Model
+#' 
+#' Once the user has finished exploring different kernel regressions via \code{\link{explore_kplcr_models}} or
+#' \code{\link{explore_kplr_models}} and has estimated future performance on the test data via 
+#' \code{\link{eval_winning_r_model_on_test_data}} or \code{\link{eval_winning_lr_model_on_test_data}},
+#' we now build the final kernel model using all the data from \code{X}, \code{y}.
+#' 
+#' @param explore_kpclr_or_kpcr 	The object built from \code{explore_kplr_models} or \code{explore_kplcr_models}.
+#' @return 							The model corresponding to the \code{winning_kernel_num} and the 
+#' 									\code{winning_rho_num} housed in the explore object.
+#' 
+#' @author 							Adam Kapelner and Justin Bleich
+#' @export
+build_final_kpclr_or_kpcr_model = function(explore_kpclr_or_kpcr){
+	#pull out the winning kernel and rebuild this kernel on both the training and validation data
+	winning_kernel_info = explore_kpclr_or_kpcr$kernel_list[[explore_kpclr_or_kpcr$winning_kernel_num]]
+	X = rbind(explore_kpclr_or_kpcr$X_train, explore_kpclr_or_kpcr$X_validate, explore_kpclr_or_kpcr$X_test)
+	kpca = build_kpca_object(X, winning_kernel_info$kernel_type, winning_kernel_info$params)
+	y = c(explore_kpclr_or_kpcr$y_train, explore_kpclr_or_kpcr$y_validate, explore_kpclr_or_kpcr$y_test)
+	#build the model and return
+	if (class(explore_kpclr_or_kpcr) == "explore_kpclr"){		
+		weights = weights_for_kpclr(y, explore_kpclr_or_kpcr$fn_cost / explore_kpclr_or_kpcr$fp_cost)
+		kpclr(kpca, y, frac_var = explore_kpclr_or_kpcr$rho_seq[explore_kpclr_or_kpcr$winning_rho_num], weights = weights, family = explore_kpclr_or_kpcr$family)	
+	} else if (class(explore_kpclr_or_kpcr) == "explore_kpclr"){
+		kpcr(kpca, y, frac_var = explore_kpclr_or_kpcr$rho_seq[explore_kpclr_or_kpcr$winning_rho_num])
+	}	
+}
--- kernReg/R/explore_kpcr_and_kpclr_models.R
+++ kernReg/R/explore_kpcr_and_kpclr_models.R
@@ -21,7 +21,8 @@
 #' @param split_props		When splitting the data into training, validation and test sets respectively, what 
 #' 							proportions are assigned to each set? This must be numeric of size 3 and the numbers
 #' 							will be normalized to sum to one. The default split is uniform (1/3, 1/3, 1/3).  
-#' @param rho_seq			A collection of proportions of the variance explained of the kernel matrix to use.
+#' @param rho_seq			A collection of proportions of the variance explained of the kernel matrix to use. The default
+#' 							is 30\%, 35\%, ..., 95\%.
 #' @param fn_cost			The target cost of a false negative (defaults to 1). Together with \code{fp_cost},
 #' 							this will inform the algorithm of the desired ratio of costs.
 #' @param fp_cost			The target cost of a false positive (defaults to 1). Together with \code{fn_cost},
@@ -38,7 +39,8 @@
 #' @param fn_max_cost		The maximum cost of a false negative. Together with \code{fp_min_cost}, this will inform
 #' 							the algorithm of the minimum cost ratio of fp:fn. If left to the default \code{NULL}, the minimum
 #' 							cost ratio will be 25\% less than the desired cost ratio. 
-#' @param family			The family parameter to be passed to the glm function. Default is "binomial." Note that with this default, AIC calculations are not possible.
+#' @param family			The family parameter to be passed to the glm function. Default is "binomial." Note that when family 
+#' 							is set to "quasibinomial," AIC calculations are not possible.
 #' @param num_cores			The number of cores to use in parallel during computation.
 #' 
 #' @return 					An object of class \code{explore_kplcr} which is a list housing the results of the procedure
@@ -51,7 +53,7 @@ explore_kpclr_models = function(X, y,
 								kernel_list = NULL, 
 								seed = 0, 
 								split_props = c(1/3, 1/3, 1/3), 
-								rho_seq = seq(0.05, 0.95, 0.05), 
+								rho_seq = seq(from = 0.30, to = 0.95, by = 0.05),
 								fn_cost = 1,
 								fp_cost = 1,
 								fp_max_cost = NULL,
@@ -68,7 +70,7 @@ explore_kpclr_models = function(X, y,
 		min_fn_fp_ratio = fp_min_cost / fn_max_cost
 		max_fn_fp_ratio = fp_max_cost / fn_min_cost
 	} else {
-		stop("Either fn_min_cost, fn_max_cost, fp_min_cost, fp_max_cost all have to be specified or none have to specified.")
+		stop("Either fn_min_cost, fn_max_cost, fp_min_cost, fp_max_cost *all* have to be specified or *none* have to specified.")
 	}							
 
 	obj = explore_common(X, y, kernel_list, seed, split_props, rho_seq)
@@ -163,6 +165,7 @@ explore_kpclr_models = function(X, y,
 	obj$winning_kernel = all_kernels[[winning_kernel_num]]
 	obj$winning_kernel_num = winning_kernel_num
 	obj$winning_rho_num = winning_rho_num
+	obj$winning_rho = rho_seq[winning_rho_num]
 	class(obj) = "explore_kpclr"
 	obj
 }
@@ -190,7 +193,8 @@ explore_kpclr_models = function(X, y,
 #' @param split_props		When splitting the data into training, validation and test sets respectively, what 
 #' 							proportions are assigned to each set? This must be numeric of size 3 and the numbers
 #' 							will be normalized to sum to one. The default split is uniform (1/3, 1/3, 1/3).  
-#' @param rho_seq			A collection of proportions of the variance explained of the kernel matrix to use.
+#' @param rho_seq			A collection of proportions of the variance explained of the kernel matrix to use. The default
+#' 							is 30\%, 35\%, ..., 95\%.
 #' @param num_cores			The number of cores to use in parallel during computation.
 #' 
 #' 
@@ -204,7 +208,7 @@ explore_kpcr_models = function(X, y,
 		kernel_list = NULL, 
 		seed = 0, 
 		split_props = c(1/3, 1/3, 1/3), 
-		rho_seq = seq(0.05, 0.95, 0.05),
+		rho_seq = seq(from = 0.30, to = 0.95, by = 0.05),
 		num_cores = 1){
 	
 	obj = explore_common(X, y, kernel_list, seed, split_props, rho_seq)
@@ -262,6 +266,7 @@ explore_kpcr_models = function(X, y,
 	obj$winning_kernel = all_kernels[[winning_kernel_num]]
 	obj$winning_kernel_num = winning_kernel_num
 	obj$winning_rho_num = winning_rho_num
+	obj$winning_rho = rho_seq[winning_rho_num]
 	class(obj) = "explore_kpcr"
 	obj
 }
@@ -373,114 +378,6 @@ explore_common = function(X, y, kernel_list, seed, split_props, rho_seq){
 	obj
 }
 
-#' Evaluate Test Data
-#'
-#' After a "satisfactory" model is selected by the user using the \code{explore_kpcr_models} function,
-#' we now predict on the test data to get a glimpse into this model's future out-of-sample performance. 
-#' Warning: once this is done, you cannot "go back" and "try" to assess performance on new kernels as this
-#' would then be snooping. Run this function when you are ready to close the books on this data set and never
-#' look back.
-#' 
-#' @param explore_kpclr_obj 		This object is built from \code{explore_kplcr_models}. We assume the user has updated 
-#' 									this object with a satisfactory model by settings \code{winning_kernel_num} to denote 
-#' 									which kernel is selected for the final model and setting \code{winning_rho_num} to 
-#' 									denote which proportion of the variance of the kernel matrix is selected for the 
-#' 									final model.
-#' 
-#' @return 							An expanded \code{explore_kpclr} list object with new entries
-#' 									that contain information about the performance of the final model on the
-#' 									test data: \code{test_confusion}, the confusion matrix of the test data; 
-#' 									\code{test_confusion_proportions}, the confusion matrix of the test data
-#' 									as proportions of the number of test observations; \code{test_misclassification_error}, 
-#' 									the total misclassification error of the test data and \code{test_weighted_cost},  
-#' 									the cost of the errors made as defined by the \code{fn_cost} and \code{fp_cost}
-#' 									specified by the user when constructing the model via \code{explore_kplcr_models}.
-#' 
-#' @seealso 						code{explore_kplcr_models}
-#' @author 							Adam Kapelner and Justin Bleich
-#' @export
-eval_winning_lr_model_on_test_data = function(explore_kpclr_obj){
-	#predict the model on test data and build a confusion matrix
-	#predict the model on training and validation data
-	winning_kernel_info = explore_kpclr_obj$kernel_list[[explore_kpclr_obj$winning_kernel_num]]
-	X_train_and_validate = rbind(explore_kpclr_obj$X_train, explore_kpclr_obj$X_validate)
-	kpca = build_kpca_object(X_train_and_validate, winning_kernel_info$kernel_type, winning_kernel_info$params)
-	y_train_and_validate = c(explore_kpclr_obj$y_train, explore_kpclr_obj$y_validate)
-	weights = weights_for_kpclr(y_train_and_validate, explore_kpclr_obj$fn_cost / explore_kpclr_obj$fp_cost)
-	winning_model = kpclr(kpca, y_train_and_validate, frac_var = explore_kpclr_obj$rho_seq[explore_kpclr_obj$winning_rho_num], weights = weights, family = explore_kpclr_obj$family)	
-	y_test_hat = predict(winning_model, explore_kpclr_obj$X_test)
-	test_confusion = table(explore_kpclr_obj$y_test, ifelse(y_test_hat > 0.5, 1, 0)) ###FIX LATER!!!
-	#pass back the data
-	explore_kpclr_obj[["test_confusion"]] = test_confusion
-	explore_kpclr_obj[["test_confusion_proportions"]] = test_confusion / explore_kpclr_obj$n_test
-	explore_kpclr_obj[["test_misclassification_error"]] = (test_confusion[1, 2] + test_confusion[2, 1]) / explore_kpclr_obj$n_test
-	explore_kpclr_obj[["test_weighted_cost"]] = test_confusion[2, 1] * fn_cost + test_confusion[1, 2] * fp_cost
-	explore_kpclr_obj
-}
-
-#' Evaluate Test Data
-#'
-#' After a "satisfactory" model is selected by the user using the \code{explore_kpcr_models} function,
-#' we now predict on the test data to get a glimpse into this model's future out-of-sample performance. 
-#' Warning: once this is done, you cannot "go back" and "try" to assess performance on new kernels as this
-#' would then be snooping. Run this function when you are ready to close the books on this data set and never
-#' look back.
-#' 
-#' @param explore_kpcr 				This object is built from \code{explore_kpcr_models}. We assume the user has updated 
-#' 									this object with a satisfactory model by settings \code{winning_kernel_num} to denote 
-#' 									which kernel is selected for the final model and setting \code{winning_rho_num} to 
-#' 									denote which proportion of the variance of the kernel matrix is selected for the 
-#' 									final model.
-#'
-#' @return 							An expanded \code{explore_kpcr} list object with new entries
-#' 									that contain information about the performance of the final model on the
-#' 									test data: \code{L2_err}, the sum of squared error; \code{rmse}, the root 
-#' 									mean squared error and \code{L1_err}, the sum of absolute error.
-#' 
-#' @seealso 						\code{\link{explore_kplr_models}}
-#' @author 							Adam Kapelner and Justin Bleich
-#' @export
-eval_winning_r_model_on_test_data = function(explore_kpcr){
-	#predict the model on training and validation data
-	winning_kernel_info = explore_kpcr$kernel_list[[explore_kpcr$winning_kernel_num]]
-	X_train_and_validate = rbind(explore_kpcr$X_train, explore_kpcr$X_validate)
-	kpca = build_kpca_object(X_train_and_validate, winning_kernel_info$kernel_type, winning_kernel_info$params)
-	y_train_and_validate = c(explore_kpcr$y_train, explore_kpcr$y_validate)
-	winning_model = kpcr(kpca, y_train_and_validate, frac_var = explore_kpcr$rho_seq[explore_kpcr$winning_rho_num])
-	y_test_hat = predict(winning_model, explore_kpcr$X_test)
-	#pass back the performance data
-	sse = sum((y_test_hat - exploration_kpclr$y_test)^2)
-	exploration_kpclr[["L2_err"]] = sse
-	exploration_kpclr[["rmse"]] = sqrt(sse / explore_kpcr$n_test)	
-	exploration_kpclr[["L1_err"]] = sum(abs(y_test_hat - explore_kpcr$y_test))
-	exploration_kpclr
-}
 
 
-#' Create Final Kernel Model
-#' 
-#' Once the user has finished exploring different kernel regressions via \code{\link{explore_kplcr_models}} or
-#' \code{\link{explore_kplr_models}} and has estimated future performance on the test data via 
-#' \code{\link{eval_winning_r_model_on_test_data}} or \code{\link{eval_winning_lr_model_on_test_data}},
-#' we now build the final kernel model using all the data from \code{X}, \code{y}.
-#' 
-#' @param explore_kpclr_or_kpcr 	The object built from \code{explore_kplr_models} or \code{explore_kplcr_models}.
-#' @return 							The model corresponding to the \code{winning_kernel_num} and the 
-#' 									\code{winning_rho_num} housed in the explore object.
-#' 
-#' @author 							Adam Kapelner and Justin Bleich
-#' @export
-build_final_kpclr_or_kpcr_model = function(explore_kpclr_or_kpcr){
-	#pull out the winning kernel and rebuild this kernel on both the training and validation data
-	winning_kernel_info = explore_kpclr_or_kpcr$kernel_list[[explore_kpclr_or_kpcr$winning_kernel_num]]
-	X = rbind(explore_kpclr_or_kpcr$X_train, explore_kpclr_or_kpcr$X_validate, explore_kpclr_or_kpcr$X_test)
-	kpca = build_kpca_object(X, winning_kernel_info$kernel_type, winning_kernel_info$params)
-	y = c(explore_kpclr_or_kpcr$y_train, explore_kpclr_or_kpcr$y_validate, explore_kpclr_or_kpcr$y_test)
-	#build the model and return
-	if (class(explore_kpclr_or_kpcr) == "explore_kpclr"){		
-		weights = weights_for_kpclr(y, explore_kpclr_or_kpcr$fn_cost / explore_kpclr_or_kpcr$fp_cost)
-		kpclr(kpca, y, frac_var = explore_kpclr_or_kpcr$rho_seq[explore_kpclr_or_kpcr$winning_rho_num], weights = weights, family = explore_kpclr_or_kpcr$family)	
-	} else if (class(explore_kpclr_or_kpcr) == "explore_kpclr"){
-		kpcr(kpca, y, frac_var = explore_kpclr_or_kpcr$rho_seq[explore_kpclr_or_kpcr$winning_rho_num])
-	}	
-}
+
