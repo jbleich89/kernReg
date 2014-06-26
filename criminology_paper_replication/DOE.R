8d777c8a5e2833c808785005426c2ab296773835
jbleich89 <jbleich89@gmail.com>
2014-06-25 20:42:10 -0400

    updated DOE

--- criminology_paper_replication/DOE.R
+++ criminology_paper_replication/DOE.R
@@ -12,28 +12,60 @@ samp_ix = sample(1 : nrow(full_data), 1500, F)
 X = full_data[samp_ix, vars_list[-19]] ##vars_list is loaded from above 
 y = full_data$Response[samp_ix]
 
-
 data = red_data[samp_ix, ]
-y = ifelse(y == "Graduate", 1, 0)
-
 Xd = as.matrix(dummify_data(X))
 dim(Xd)
 length(y)
 summary(Xd)
 colnames(Xd)
 
-kernel_list = list()
-# kernel_list[[1]] = list(kernel_type = "anova", params = c(.1, 3))
-kernel_list[[1]] = list(kernel_type = "anova", params = c(1, 3))
-kernel_list[[2]] = list(kernel_type = "anova", params = c(10, 3))
-#kernel_list[[3]] = list(kernel_type = "anova", params = c(100, 3))
+# kernel_list = list()
+# # kernel_list[[1]] = list(kernel_type = "anova", params = c(.1, 3))
+# kernel_list[[1]] = list(kernel_type = "anova", params = c(1, 3))
+# kernel_list[[2]] = list(kernel_type = "anova", params = c(10, 3))
+# kernel_list[[3]] = list(kernel_type = "anova", params = c(100, 3))
 
-explore_kpclr_obj = explore_kpclr_models(Xd, y, kernel_list = kernel_list, fn_cost = 5, fp_cost = 1)
+kernel_list = list()
+kernel_list[[1]] = list(kernel_type = "anova", params = c(0.1, 2))
+kernel_list[[2]] = list(kernel_type = "anova", params = c(1, 2))
+kernel_list[[3]] = list(kernel_type = "anova", params = c(10, 2))
+kernel_list[[4]] = list(kernel_type = "anova", params = c(100, 2))
+kernel_list[[5]] = list(kernel_type = "anova", params = c(1000, 2))
+kernel_list[[6]] = list(kernel_type = "anova", params = c(10000, 2))
+kernel_list[[7]] = list(kernel_type = "anova", params = c(0.1, 3))
+kernel_list[[8]] = list(kernel_type = "anova", params = c(1, 3))
+kernel_list[[9]] = list(kernel_type = "anova", params = c(10, 3))
+kernel_list[[10]] = list(kernel_type = "anova", params = c(100, 3))
+kernel_list[[11]] = list(kernel_type = "anova", params = c(1000, 3))
+kernel_list[[12]] = list(kernel_type = "anova", params = c(10000, 3))
+
+explore_kpclr_obj = explore_kpclr_models(Xd, y, kernel_list = kernel_list, fn_cost = 5, fp_cost = 1, fn_max_cost = 6, fn_min_cost = 4, fp_max_cost = 1, fp_min_cost = 1)
 plot(explore_kpclr_obj, quantile_cwe_to_display = 0.99, plot_tile_cols = 2)
 explore_kpclr_obj = eval_winning_lr_model_on_test_data(explore_kpclr_obj)
+explore_kpclr_obj$test_confusion
+explore_kpclr_obj$fn_over_fp_validation_results
+
+
+explore_kpclr_obj$winning_rho_num=3
 
 adot = anovadot(10,3)
 k = kernelMatrix(kernel=adot, x=Xd)
 max(k)
 min(k)
 kpca(k)
+
+Xrf = rbind(explore_kpclr_obj$X_train, explore_kpclr_obj$X_validate)
+yrf= c(explore_kpclr_obj$y_train, explore_kpclr_obj$y_validate)
+table(yrf)
+
+rf = randomForest(Xrf, as.factor(yrf), sampsize = c(130, 175))
+prf = predict(rf, explore_kpclr_obj$X_test)
+table(explore_kpclr_obj$y_test, prf)
+260*.67
+
+kpca_obj = build_kpca_object(Xrf, kernel_type = "anova", c(100,3))
+weights = weights_for_kpclr(yrf, fn_to_fp_ratio = 5)
+mod = kpclr(kpca_obj, yrf, frac_var = .825, weights = weights)
+preds = predict(mod, new_data = explore_kpclr_obj$X_test)
+table(explore_kpclr_obj$y_test, preds > .5)
+
